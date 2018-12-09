df94c8a6de5e9cd1a151bc0074eb894f16401742
Adam Lasak <adam.lasak@tieto.com>
2018-12-09 20:36:46 +0100

    class and style bindings

---
+++ src/components/ClassAndStyleBindings.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="class-and-style-bindings">
+    <span 
+      class="output-box"
+      v-bind:class="{ active: isActive, 'text-danger': hasError }"
+    >
+      Today is: {{ new Date().toLocaleString() }}
+    </span>
+
+    <button 
+      v-on:click="hasError = !hasError" 
+    >
+      change danger
+    </button><br>
+
+    <span 
+      :class="outputBoxChangeStatus"
+    >
+      Another text...
+    </span><br>
+    <div v-bind:style="grayCube"></div>
+    <div v-bind:style="[grayCube, {backgroundColor: '#ccc'}]"></div>
+    <div v-bind:style="[grayCube, {backgroundColor: '#eee'}]"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ClassAndStyleBindings',
+    data: function() {
+      return {
+        hasError: null,
+        isActive: true,
+        grayCube: {
+          width: '50px',
+          height: '50px',
+          float: 'left',
+          'background-color': 'gray', // either kebab-case
+          backgroundColor: '#aaa', // or camelCase
+        },
+      };
+    },
+    
+    computed: {
+      // assigning particular class to element is mostly done via computed
+      outputBoxChangeStatus: function() {
+        // we can also return an array of classes
+        return {
+          active: this.isActive && this.hasError,
+          'text-danger': this.hasError,
+        }
+      }
+    },
+
+  }
+</script>
+
+<style scoped>
+.output-box {
+  padding: 5px;
+  color: black;
+}
+
+.active {
+  border: 2px solid gainsboro;
+}
+
+.text-danger {
+  color: red;
+}
+</style>
--- src/components/TestingVueFeatures.vue
+++ src/components/TestingVueFeatures.vue
@@ -2,19 +2,22 @@
   <div id="testing-vue-features">
     <h2>Testing Vue Features</h2><br>
     <BasicFeatures></BasicFeatures><hr>
-    <PropertiesAndWatchers></PropertiesAndWatchers>
+    <PropertiesAndWatchers></PropertiesAndWatchers><hr>
+    <ClassAndStyleBingings></ClassAndStyleBingings><hr>
   </div>
 </template>
 
 <script>
 import BasicFeatures from "./BasicFeatures.vue";
 import PropertiesAndWatchers from './PropertiesAndWatchers.vue';
+import ClassAndStyleBingings from './ClassAndStyleBindings.vue';
 
 export default {
   name: "TestingVueFeatures",
   components: {
     BasicFeatures,
-    PropertiesAndWatchers
+    PropertiesAndWatchers,
+    ClassAndStyleBingings,
   }
 };
 </script>
